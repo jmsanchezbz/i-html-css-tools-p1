5fb72c8c3dfa19cdcf5e182db1427003d7a66011
Jose Manuel Sánchez Báez <jmsanchezbz@uoc.edu>
2024-04-14 20:52:54 +0200

    Added category.html + improvements detail.html

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -2,7 +2,6 @@ const name = "Queen";
 console.log(`Hello ${name}`);
 
 const applyActiveClassToMenuElement = (href) => {
-  console.log("executing...");
   const menuLinks = document.querySelectorAll(".menu .ulist__link");
 
   menuLinks.forEach((e) => {
@@ -10,5 +9,23 @@ const applyActiveClassToMenuElement = (href) => {
       e.parentNode.classList.add("ulist__element--active");
       return;
     }
+  });
+};
+
+const loadYoutubeSong = (event) => {
+  const yt_link = "https://www.youtube-nocookie.com/embed/";
+
+  const iframe = document.querySelector("iframe.video--fluid");
+  const li = event.target.closest("li");
+  const videocode = li.getAttribute('yt-song');
+
+  iframe.src = yt_link + videocode;
+
+};
+
+const addListenersToSongs = () => {
+  const lis = document.querySelectorAll(".songs-list ol>li>a");
+  lis.forEach((e) => {
+    e.addEventListener("click", loadYoutubeSong);
   });
 };
